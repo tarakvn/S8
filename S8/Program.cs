2ca26fc70122610e729f24f082eac9ec4c7a6326
tarakavian84@gmail.com <tarakavian84@gmail.com>
2022-05-08 19:35:10 +0430

    Add project files.

---
+++ S8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the radius: ");
+            int radius = int.Parse(Console.ReadLine());
+            double Area = radius * radius * Math.PI;
+            double peri = radius * 2 * Math.PI;
+            Console.WriteLine("The area of the circle is " + Area);
+            Console.WriteLine("the perimeter of circle is " + peri);
+            Console.ReadKey();
+        }
+    }
+}
